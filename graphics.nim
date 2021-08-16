b121737bc87a07486d5855f11c1f2bc89cb1b5c2
Corey Lehman <coreylehman@me.com>
2021-08-16 16:27:48 -0400

    Making some progress abstracting buffers.

--- graphics.nim
+++ graphics.nim
@@ -1,68 +1,57 @@
-#import graphics/buffer
+import
+  opengl,
+  ezwin/window,
+  graphics/[context, buffer, shader]
 
-# expandMacros:
-#   initVertex(
-#     [0.5'f32, -0.5, 0.0],
-#     [0.0],
-#     [5, 10],
-#   )
-# echo vertex.numBytes
-# for attribute in vertex.attributes:
-#   echo $attribute.typ & ": " & $attribute.count
+const vertexShader = """
+#version 330 core
+layout(location = 0) in vec4 position;
+void main() {
+  gl_Position = position;
+}
+"""
 
+const fragmentShader = """
+#version 330 core
+layout(location = 0) out vec4 color;
+uniform vec4 u_Color;
+void main() {
+  color = u_Color;
+}
+"""
 
+when isMainModule:
+  var wnd = newWindow(
+    title = "Test Window",
+    x = 2.0,
+    y = 2.0,
+    width = 4.0,
+    height = 4.0,
+  )
 
-# import
-#   opengl,
-#   ezwin/window,
-#   graphics/[context, shader]
+  var ctx = initContext(wnd.handle)
 
-# const vertexShader = """
-# #version 330 core
-# layout(location = 0) in vec4 position;
-# void main() {
-#   gl_Position = position;
-# }
-# """
+  let shdr = createShader(vertexShader, fragmentShader)
+  glUseProgram(shdr)
 
-# const fragmentShader = """
-# #version 330 core
-# layout(location = 0) out vec4 color;
-# uniform vec4 u_Color;
-# void main() {
-#   color = u_Color;
-# }
-# """
+  let colorLocation = glGetUniformLocation(shdr, "u_Color")
+  glUniform4f(colorLocation, 0.2, 0.3, 0.8, 1.0)
 
-# when isMainModule:
-#   var wnd = newWindow(
-#     title = "Test Window",
-#     x = 2.0,
-#     y = 2.0,
-#     width = 4.0,
-#     height = 4.0,
-#   )
+  var vertexBuffer = initBuffer()
+  vertexBuffer.data = [
+    -0.5'f32, -0.5, 0.0,
+    0.0, 0.5, 0.0,
+    0.5, -0.5, 0.0,
+  ]
+  vertexBuffer.layout = [
+    Attribute(count: 3, typ: GfxDataType.F32),
+  ]
 
-#   var ctx = initContext(wnd.handle)
+  while not wnd.shouldClose:
+    wnd.pollEvents()
+    glClear(GL_COLOR_BUFFER_BIT)
+    glDrawArrays(GL_TRIANGLES, 0, 3)
+    # glDrawElements(GL_TRIANGLES, indices.len.GLsizei, GL_UNSIGNED_INT, nil)
+    ctx.swapBuffers()
 
-#   let shdr = createShader(vertexShader, fragmentShader)
-#   glUseProgram(shdr)
-
-#   let colorLocation = glGetUniformLocation(shdr, "u_Color")
-#   glUniform4f(colorLocation, 0.2, 0.3, 0.8, 1.0)
-
-#   var vertices = initBuffer(
-#     [-0.5'f32, -0.5, 0.0],
-#     [0.0'f32, 0.5, 0.0],
-#     [0.5'f32, -0.5, 0.0],
-#   )
-#   vertices.defineAttributes()
-
-#   while not wnd.shouldClose:
-#     wnd.pollEvents()
-#     glClear(GL_COLOR_BUFFER_BIT)
-#     glDrawArrays(GL_TRIANGLES, 0, 3)
-#     # glDrawElements(GL_TRIANGLES, indices.len.GLsizei, GL_UNSIGNED_INT, nil)
-#     ctx.swapBuffers()
-
-#   glDeleteProgram(shdr)
+  glDeleteProgram(shdr)
--- graphics/buffer.nim
+++ graphics/buffer.nim
@@ -1,5 +1,6 @@
-# import opengl
-import std/macros
+import
+  # std/macros,
+  opengl
 
 type
   GfxDataType* = enum
@@ -12,24 +13,19 @@ type
     count*: int
     typ*: GfxDataType
 
-  Vertex* = object
-    attributes*: seq[Attribute]
-    data*: pointer
+  Buffer* = object
+    openGlId*: GLuint
 
-  # Buffer* = object
-  #   vertices*: seq[Vertex]
-  #   openGlId: GLuint
-
-# proc toGlEnum(dataType: GfxDataType): GLenum =
-#   case dataType:
-#   of I8: cGL_BYTE
-#   of U8: cGL_UNSIGNED_BYTE
-#   of I16: cGL_SHORT
-#   of U16: cGL_UNSIGNED_SHORT
-#   of I32: cGL_INT
-#   of U32: GL_UNSIGNED_INT
-#   of F32: cGL_FLOAT
-#   of F64: cGL_DOUBLE
+proc toGlEnum(dataType: GfxDataType): GLenum =
+  case dataType:
+  of I8: cGL_BYTE
+  of U8: cGL_UNSIGNED_BYTE
+  of I16: cGL_SHORT
+  of U16: cGL_UNSIGNED_SHORT
+  of I32: cGL_INT
+  of U32: GL_UNSIGNED_INT
+  of F32: cGL_FLOAT
+  of F64: cGL_DOUBLE
 
 proc toGfxDataType*(typ: typedesc): GfxDataType =
   if typ is int8: I8
@@ -45,17 +41,6 @@ proc toGfxDataType*(typ: typedesc): GfxDataType =
   elif typ is float: F64
   else: raise newException(IOError, "Improper type for toGfxDataType.")
 
-# proc toNimType(typ: GfxDataType): typedesc =
-#   case typ:
-#   of I8: int8
-#   of U8: uint8
-#   of I16: int16
-#   of U16: uint16
-#   of I32: int32
-#   of U32: uint32
-#   of F32: float32
-#   of F64: float64
-
 proc numBytes*(typ: GfxDataType): int =
   case typ:
   of I8: 1
@@ -70,124 +55,124 @@ proc numBytes*(typ: GfxDataType): int =
 proc numBytes*(attribute: Attribute): int =
   attribute.count * attribute.typ.numBytes
 
-proc numBytes*(vertex: Vertex): int =
-  for attribute in vertex.attributes:
+proc vertexNumBytes*(attributes: openArray[Attribute]): int =
+  for attribute in attributes:
     result += attribute.numBytes
 
+proc select*(buffer: Buffer) =
+  glBindBuffer(GL_ARRAY_BUFFER, buffer.openGlId)
 
-
-# proc numBytes*(buffer: Buffer): int =
-#   for vertex in buffer.vertices:
-#     result += vertex.numBytes
-
-# proc select*(buffer: var Buffer) =
-#   glBindBuffer(
-#     target = GL_ARRAY_BUFFER,
-#     buffer = buffer.openGlId,
-#   )
-#   glBufferData(
-#     target = GL_ARRAY_BUFFER,
-#     size = buffer.numBytes,
-#     data = buffer.vertices.addr,
-#     usage = GL_STATIC_DRAW,
-#   )
-
-# proc defineAttributes*(buffer: Buffer) =
-#   if buffer.vertices.len > 0:
-#     for vertex in buffer.vertices:
-#       let vertexNumBytes = vertex.numBytes
-#       var byteOffset = 0
-#       for i, attribute in vertex.attributes:
-#         glEnableVertexAttribArray(i.GLuint)
-#         glVertexAttribPointer(
-#           index = i.GLuint,
-#             # the 0 based index of the attribute
-#           size = attribute.count.GLuint,
-#             # the number of values in the attribute
-#           `type` = attribute.typ.toGlEnum,
-#             # the type of value present in the attribute
-#           normalized = GL_FALSE,
-#             # normalize the values from 0 to 1 on the gpu
-#           stride = vertexNumBytes,
-#             # byte offset of each vertex
-#           `pointer` = cast[pointer](byteOffset),
-#             # byte offset of the start of the attribute, cast as a pointer
-#         )
-#         byteOffset += attribute.numBytes
-
-# proc `=destroy`*(buffer: var Buffer) =
-#   glDeleteBuffers(1, buffer.openGlId.addr)
-
-# proc initBuffer*(): Buffer =
-#   glGenBuffers(1, result.openGlId.addr)
-
-
-
-# proc `=destroy`*(vertex: var Vertex) =
-#   dealloc(vertex.data)
-
-macro initVertex*(args: varargs[untyped]): untyped =
-  var
-    stmts = nnkStmtListExpr.newTree()
-    vertexSym = genSym(nskVar, "vertex")
-    # byteOffsetSym = genSym(nskVar, "byteOffset")
-    numArgs = args.len
-    attributeSyms = newSeq[NimNode](numArgs)
-
-  for i, arg in args:
-    let attributeSym = genSym(nskLet, "attribute")
-    attributeSyms[i] = attributeSym
-    stmts.add quote do:
-      let `attributeSym` = `arg`
-
-  stmts.add quote do:
-    var `vertexSym` = Vertex(
-      attributes: newSeq[Attribute](`numArgs`)
+proc `data=`*[T](buffer: Buffer, data: openArray[T]) =
+  if data.len > 0:
+    buffer.select()
+    var dataSeq = newSeq[T](data.len)
+    for i, v in data:
+      dataSeq[i] = v
+    glBufferData(
+      target = GL_ARRAY_BUFFER,
+      size = dataSeq.len * dataSeq[0].sizeof,
+      data = dataSeq[0].addr,
+      usage = GL_STATIC_DRAW,
     )
 
-  for i, arg in args:
-    let attributeSym = attributeSyms[i]
-    stmts.add quote do:
-      `vertexSym`.attributes[`i`] = Attribute(
-        count: `attributeSym`.len,
-        typ: `attributeSym`[0].typeof.toGfxDataType,
-      )
+proc `data=`*[T](buffer: Buffer, data: var seq[T]) =
+  if data.len > 0:
+    buffer.select()
+    glBufferData(
+      target = GL_ARRAY_BUFFER,
+      size = data.len * data[0].sizeof,
+      data = data[0].addr,
+      usage = GL_STATIC_DRAW,
+    )
 
-  stmts.add quote do:
-    `vertexSym`.data = alloc(`vertexSym`.numBytes)
-    # var `byteOffsetSym` = 0
+proc setLayout(buffer: var Buffer, attributes: openArray[Attribute]) =
+  let vertexNumBytes = attributes.vertexNumBytes
+  var byteOffset = 0
+  for i, attribute in attributes:
+    glEnableVertexAttribArray(i.GLuint)
+    glVertexAttribPointer(
+      index = i.GLuint,
+        # the 0 based index of the attribute
+      size = attribute.count.GLint,
+        # the number of values in the attribute
+      `type` = attribute.typ.toGlEnum,
+        # the type of value present in the attribute
+      normalized = GL_FALSE,
+        # normalize the values from 0 to 1 on the gpu
+      stride = vertexNumBytes.GLsizei,
+        # byte offset of each vertex
+      `pointer` = cast[pointer](byteOffset),
+        # byte offset of the start of the attribute, cast as a pointer
+    )
+    byteOffset += attribute.numBytes
 
-  for i, arg in args:
-    let attributeSym = attributeSyms[i]
-    stmts.add quote do:
-      var dataPtr = cast[ptr `attributeSym`.typeof](`vertexSym`.data)
-      dataPtr[] = `attributeSym`
+proc `layout=`*(buffer: var Buffer, attributes: openArray[Attribute]) =
+  buffer.setLayout(attributes)
+
+proc `=destroy`*(buffer: var Buffer) =
+  glDeleteBuffers(1, buffer.openGlId.addr)
+
+proc initBuffer*(): Buffer =
+  glGenBuffers(1, result.openGlId.addr)
 
 
-  # for i, arg in args:
-  #   let attributeSym = attributeSyms[i]
-  #   stmts.add quote do:
-  #     var dataPtr = cast[ptr `attributeSym`.typeof](cast[int](`vertexSym`.data) + `byteOffsetSym`)
-  #     dataPtr[] = `attributeSym`
-  #   if i < args.len - 1:
-  #     stmts.add quote do:
-  #       `byteOffsetSym` += `attributeSym`.sizeof
 
-  stmts.add quote do:
-    `vertexSym`
 
-  nnkBlockExpr.newTree(newEmptyNode(), stmts)
+# macro makeAttributes*(args: varargs[untyped]): untyped =
+#   var
+#     stmts = nnkStmtListExpr.newTree()
+#     vertexSym = genSym(nskVar, "vertex")
+#     # byteOffsetSym = genSym(nskVar, "byteOffset")
+#     numArgs = args.len
+#     attributeSyms = newSeq[NimNode](numArgs)
 
-when isMainModule:
-  let vertex = initVertex(
-    [0.5'f32, -0.5, 0.0],
-    # [0.0],
-    # [5, 10],
-  )
+#   for i, arg in args:
+#     let attributeSym = genSym(nskLet, "attribute")
+#     attributeSyms[i] = attributeSym
+#     stmts.add quote do:
+#       let `attributeSym` = `arg`
 
-  let vertexData = cast[ptr [0.5'f32, -0.5, 0.0].typeof](vertex.data)
-  echo vertexData[]
+#   stmts.add quote do:
+#     var `vertexSym` = Vertex(
+#       attributes: newSeq[Attribute](`numArgs`)
+#     )
 
-  # echo vertex.numBytes
-  # for attribute in vertex.attributes:
-  #   echo $attribute.typ & ": " & $attribute.count
+#   for i, arg in args:
+#     let attributeSym = attributeSyms[i]
+#     stmts.add quote do:
+#       `vertexSym`.attributes[`i`] = Attribute(
+#         count: `attributeSym`.len,
+#         typ: `attributeSym`[0].typeof.toGfxDataType,
+#       )
+
+#   stmts.add quote do:
+#     `vertexSym`.data = alloc(`vertexSym`.numBytes)
+#     # var `byteOffsetSym` = 0
+
+#   for i, arg in args:
+#     let attributeSym = attributeSyms[i]
+#     stmts.add quote do:
+#       var dataPtr = cast[ptr `attributeSym`.typeof](`vertexSym`.data)
+#       dataPtr[] = `attributeSym`
+
+
+#   for i, arg in args:
+#     let attributeSym = attributeSyms[i]
+#     stmts.add quote do:
+#       var dataPtr = cast[ptr `attributeSym`.typeof](cast[int](`vertexSym`.data) + `byteOffsetSym`)
+#       dataPtr[] = `attributeSym`
+#     if i < args.len - 1:
+#       stmts.add quote do:
+#         `byteOffsetSym` += `attributeSym`.sizeof
+
+#   stmts.add quote do:
+#     `vertexSym`
+
+#   nnkBlockExpr.newTree(newEmptyNode(), stmts)
+
+
+# when isMainModule:
+#   echo makeAttributes(
+#     [2, float],
+#     [3, int],
+#   )
